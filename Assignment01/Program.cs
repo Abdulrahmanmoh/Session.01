54270c33e5b1ecfe2f1fecc345662a7b9fd7a122
dell <dell@DESKTOP-D4CB5ND>
2024-12-14 16:23:30 +0200

    Add project files.

---
+++ Assignment01/Program.cs
@@ -0,0 +1,171 @@
+﻿using System.ComponentModel;
+
+namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Problem.01
+            ////Write a program that allows the user to enter a number then print it.
+            //Console.WriteLine("Enter a Number");
+            //Console.WriteLine(value: "Enter a Number"); //What is the using of value
+            //double number = double.Parse(Console.ReadLine());
+            //Console.Write("The Number You Have Entered is " + number);
+            #endregion
+
+            #region Problem.02*
+
+
+
+            ///*Write C# program that converts a string to an integer,
+            //but the string contains non-numeric characters. 
+            ////And mention what will happen */
+            //Console.WriteLine("Enter a String");
+            //int convertor = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The Value is " + convertor);
+            #endregion
+
+            #region Problem.03
+
+
+            /*Write C# program that Perform a simple arithmetic operation 
+            with floating-point numbers And mention what will happen*/
+            //Console.WriteLine("Enter the First Number");
+            //float no01 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Second Number");
+            //float no02 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("The Summition Of The Two Numbers =" + (no01 + no02));
+            #endregion
+
+            #region Problem.04*
+            //Write C# program that Extract a substring from a given string
+
+            #endregion
+
+            #region Problem.05
+            /*
+            Write C# program that Assigning one value type variable
+            to another and modifying the value of one variable and 
+            mention what will happen*/
+            //int x;
+            //x = 5;
+            //int Y = x;
+            //x = 7; //Ihave changed the value of X why still printing Y with the old value?
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(Y);
+
+
+
+            #endregion
+
+            #region Problem.06
+
+
+            /*Write C# program that Assigning one reference type variable 
+             to another and modifying the object through one variable and mention what will happen*/
+            //string value01;
+            //value01 = "Hello world";
+            //string value02;
+            //value02 = value01;
+            //Console.WriteLine(value02);
+
+            #endregion
+
+            #region Problem.07
+            //Write C# program that take two string variables and print them as one variable
+            //Console.WriteLine("Enter Your First Name");
+            //string First_name = (Console.ReadLine());
+            //Console.WriteLine("Enter Your Last Name");
+            //string Last_name = (Console.ReadLine());
+            //Console.WriteLine("Your Full Name is "+ First_name+ " "+Last_name);
+            #endregion
+
+            #region Problem.08
+            /*Write a program that calculates the simple interest given the principal amount, 
+             rate of interest, and time. The formula for simple interest is 
+             Interest = (principal * rate * time ) /100.*/
+            //double Interest;
+            //double principal;
+            //double rate;
+            //double time;
+            //Console.WriteLine("Enter principal");
+            //principal = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter rate");
+            //rate = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter time");
+            //time= double.Parse(Console.ReadLine());
+            //Console.WriteLine("Interest = " + (principal * rate * time) / 100);
+            #endregion
+
+            #region Problem.09
+            //Write a program that calculates the Body Mass Index
+            //(BMI) given a person's weight in kilograms and height in meters.
+            //The formula for BMI is BMI = (Weight) / (Height * Height)
+
+            //double BMI;
+            //double Weight;
+            //double Height;
+            //Console.WriteLine("Enter Your Weight");
+            //Weight = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your Height");
+            //Height = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Your BMI = " + (Weight) / (Height * Height));
+
+            #endregion
+
+            #region Problem.10
+            //Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good.
+            //Assign the result in a variable then display the result.
+            //Assume that below 10 degrees is "Just Cold", above 30 degrees is "Just Hot",
+            //and anything else is "Just Good".
+            //Console.WriteLine("Enter the Temperature Degree");
+            // double temperature =double.Parse(Console.ReadLine());
+            // string result;
+            // result= temperature < 10 ? "Just coold" :
+            //result =  temperature > 30? "Just hot" : "Just Good";
+            // Console.WriteLine(result);
+            #endregion
+
+            #region Problem.11*
+            /*
+            Write a program that takes the date from the user and displays it
+            in various formats using string interpolation.
+            Ex:
+            Today’s date : 20 , 11 , 2001
+            Today's date : 20 / 11 / 2001
+            Today's date : 20 – 11 – 2001
+            */
+            #endregion*
+
+            #region Problem.012
+            //DateTime date = new DateTime(2024, 6, 14);
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+
+            #endregion
+
+            #region Problem.013
+            //Which of the following statements is correct about the C#.NET code snippet given below?
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            #endregion
+
+            #region Problem.014
+            ////Which of the following is the correct output for the C# code given below?
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+            #endregion
+
+            #region Problem.015
+            //What will be the output of the C# code given below?
+
+            //int num = 1, z = 5;
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+            #endregion
+        }
+    }
+}
---
+++ Session.01/Program.cs
@@ -0,0 +1,228 @@
+﻿//Solution: Is a container of projects
+//Namespace:Is a container of classes
+//Class:Is a container of Functions
+//Functions:Is a container of Codes or statments
+//Calling a function: Namespace.Class.Function()
+//File is prefered to contain only on class
+//Global Using: Group of global namespaces will be used in the project
+using System;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+namespace Session._01
+
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            #region Variables
+
+            //Console.WriteLine("Hello, World!");
+
+            ////int Number;  //declare
+            ////Number = 5; //Intialze 
+            ////Console.WriteLine(Number); //print
+
+            //int Number = 5; //declare+Intialze
+            //Console.WriteLine(Number); //Print
+
+            //int Number01 = 5, Number02 = 6, Number03 = 7;
+            //Console.WriteLine(Number01);
+            //Console.WriteLine(Number02);
+            //Console.WriteLine(Number03);
+            #endregion
+
+
+
+
+            //CLS->Common Language specification
+            //CTS->Common type systme
+
+
+            //Tyoes of data types:
+            //Value data type--> stored in stack RAM
+            //Reference data type-->stored in Heap RAM
+
+            #region value data type
+            ////1-value data type
+            ////a.declare variable
+            //int number01; //c# key word
+            //int32 number02; //bcl
+            ////b. value intializaiotn 
+            //number01 = 5;
+            //number02 = 10;
+            ////c.printing 
+            //console.writeline(number01);
+            //console.writeline(number02);
+            #endregion
+            #region Reference Data Type
+
+            Point P01;
+            // Declare Refernce From Type 'Point'
+            // P01 : Can Refer To Object From Type 'Point'
+            // 8 Bytes Will Be Allocated At Stack For The 'P01'
+            // 0 Bytes Will Be Allocated At Heap
+            P01 = new Point();
+            //New effect
+            // 1. Allocate The Number Of Required Bytes For The Object at Heap
+            // 2. Initialized The Allocated Byte With The Defaul Value of The Datatype
+            // 3. Call User-Defined Constructor if Exsits
+            // 4. Assign The Object Address To The Reference 'P01'
+
+
+            //P01.x = 5;
+            //P01.y = 10;
+            //Console.WriteLine(P01.x);
+            //Console.WriteLine(P01.y);
+            //Console.WriteLine(P01);
+            #endregion
+
+            #region object
+            //object ob01;
+            //ob01 = new object();
+            //ob01 = 1;
+            //Console.WriteLine(ob01);
+            //ob01 = 3.5;
+            //Console.WriteLine(ob01);
+            //ob01 = "Hello";
+            //Console.WriteLine(ob01);
+            //ob01 = new Point();
+            //Console.WriteLine(ob01);
+
+
+            ////TO String()
+            //Console.WriteLine(ob01.ToString());
+            ////Get hash cosw
+            //Console.WriteLine(ob01.GetHashCode());
+            #endregion
+
+            #region casting
+            //Casting: Convert From Any Datatype To Any Datatype
+            // 1. Implicit Casting
+            //int x = 5;
+            //double y = x;
+            //Console.WriteLine(y);
+
+            //// 2. Explicit Casting
+
+            //double X = 2.5;
+
+            //int Y = (int)X; // Explicit Casting (UnSafe)
+            //Console.WriteLine(Y);
+
+            //Parse
+            //Console.WriteLine(value: "Enter Your Age");
+            ////Console.ReadLine();
+            ////int age = int.Parse(Console.ReadLine());
+            ////Console.WriteLine(value: "Enter Your GPA");
+            ////double GPA= double.Parse(Console.ReadLine());
+            ////Console.WriteLine("Your Age is:" + age);
+            ////Console.WriteLine("Your GPA is" + GPA);
+
+            //Convert class
+            //Console.WriteLine(value: "Enter Your Age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your Age is:" + age);
+
+            //tryparse
+            //Console.WriteLine(value: "Enter Your Age");
+            //int Age;
+            //bool flag = int.TryParse(Console.ReadLine(), out Age);
+            //Console.WriteLine(Age);
+            //Console.WriteLine(flag);
+            #endregion
+
+
+
+            #region Fractions And Discards
+            ////double X = 1.5;
+            ////float Y = 1.5F;
+
+            ////decimal Z = 1.5m;
+
+            //int X = 1_000_000_000; // Discard
+
+            //Console.WriteLine(X);
+            #endregion
+
+            #region Operators
+
+            // 1. Unary Operators -> Works On One Operand (Variable) ++ --
+            //int X = 10;
+            // ++
+            // PreFix [Increment then print]
+            //Console.WriteLine(++X); // 11
+            //// PostFix [Print then Increment]
+            //Console.WriteLine(X++); // 10
+            //Console.WriteLine(X); // 11
+
+            // PreFix [decrement then print]
+            //Console.WriteLine( -- X); // 9
+            // PostFix [Print then decrement]
+            //Console.WriteLine(X--); // 10
+            //Console.WriteLine(X); // 9
+
+
+            // 2.Binary Operators => Works on Two Operands (Variables)
+            // + = /* %
+            //int Sum, Mul, Sub, Mod;
+            //int Number01= 2, Number02= 6;
+
+            //Sum = Number01 + Number02; // 8
+            //Mul = Number01 * Number02; // 12
+            //Sub = Number01 - Number02; // -4
+            //Mod = Number01 % Number02; // 2
+            // 23 % 3 = 2
+
+            // 3. Assignment Operators
+            //int x;
+            //x = 4;
+            //x += 2; // X = X + 2 / / 6
+            //Console.WriteLine(x);
+            //x -= 2; // X = X - 2 // 2
+            //Console.WriteLine(x);
+            //x *= 2; // X = X * 2 //8
+            //Console.WriteLine(x);
+            //x /= 2; // X = x / 2 //2
+            //Console.WriteLine(x);
+            //x %= 2; //X = X % 2 //0
+            //Console.WriteLine(x);
+
+
+            // 4. Relational Operators [Comparsion]
+
+            //int x = 6, y = 5;
+            //Console.WriteLine(x == y);
+            //Console.WriteLine(x != y);
+            //Console.WriteLine(x>y);
+            //Console.WriteLine(x > y);
+            //Console.WriteLine(x < y);
+            //Console.WriteLine(x >= y);
+            //Console.WriteLine(x <= y);
+
+            // 5. Logical Operators [AND , OR , NOT]
+            // Console.WriteLine(!false); // ! -> Not
+            //Console.WriteLine(false && true); // && -> AND Short Circuit
+            //Console.WriteLine(false | | true); // || -> OR Short Circuit
+
+            ////6. BitWise Operators
+            //Console.WriteLine(!false); // ! -> Not
+            //Console.WriteLine(false & true); // && -> AND Long Circuit
+            //Console.WriteLine(false | true); // || -> OR Long Circuit
+
+            //// 7. Ternary Operator [Conditional Operator]
+
+            //bool flag = 4 > 2;
+            //Console.WriteLine(flag);
+            //string flag = 4 < 2 ? "Cornect" : "Not Correct";
+            //Console.WriteLine(flag);
+
+
+            #endregion
+
+
+
+        }
+    }
+}
